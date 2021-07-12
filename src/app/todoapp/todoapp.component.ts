b7eb425462f143031782bce99d949e929fab3ad9
srinispice <srini.4047@gmail.com>
2021-07-12 13:09:17 +0530

    Css basic design implemented

---
+++ src/app/todoapp/todoapp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todoapp',
+  templateUrl: './todoapp.component.html',
+  styleUrls: ['./todoapp.component.scss']
+})
+export class TodoappComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
